36e8c540dbb5fdcff0ff56ce3693b841e2713729
michel petrois <michel.petrois@laposte.net>
2022-09-19 23:40:38 +0200

    creation

---
+++ venv/aff_tables.py
@@ -0,0 +1,42 @@
+import sys
+import simple_sqlite
+import magic
+import os
+
+# fonctions
+def colored(r, g, b, text):
+    return "\033[38;2;{};{};{}m{} \033[38;2;255;255;255m".format(r, g, b, text)
+
+
+def scan_file(racine):
+    liste_db = []
+    for dirpath, dirnames, filenames in os.walk(racine):
+        for fichier in filenames:
+            type_fic = magic.from_file(dirpath+"/"+fichier)
+            if type_fic[:19] == "SQLite 3.x database":
+                liste_db.append(dirpath+"/"+fichier)
+    return liste_db
+
+def list_schemas(db):
+    base_lu = simple_sqlite.SimpleSqlite(db)
+    print(base_lu.schema)
+
+# init vars
+racine = ""
+i = 1
+liste_tables = []
+color_rep = colored(194, 1, 20, " ")
+color_table = colored(185, 251, 192, " ")
+color_select = colored(255, 238, 50, " ")
+
+# recup params
+while i < len(sys.argv):
+    print('i:', i)
+    if i == 1:
+        racine = sys.argv[i]
+    else:
+        liste_tables.append(sys.argv[i])
+    i = i + 1
+
+for base in scan_file(racine):
+    list_schemas(base)
